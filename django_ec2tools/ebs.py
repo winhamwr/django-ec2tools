657b699fbd963382eab5274a21f5d082380458fd
Wes Winham <wes@wes-worktop.(none)>
2009-06-25 17:14:30 -0400

    Stubbed out the take_snapshot method plan with comments

--- django_ec2tools/ebs.py
+++ django_ec2tools/ebs.py
@@ -3,4 +3,23 @@ def take_snapshot():
     """
     Take a snapshot of all configured EBS volumes.
     """
+    # Read the configurations to create a boto ec2 connection
+
+    # Read the instance.ini config file and create boto ec2 volumes for each
+
+    # Connect to the database using a django cursor
+
+    # For each volume
+        # Flush the tables and grab a READ LOCK
+
+        # Freeze the xfs file system
+
+        # Call create_snapshot
+
+        # Unfreeze the xfs file system
+
+        # Unlock the table
+
+    # Log all fo the snapshot info
+
     pass
