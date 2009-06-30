f6dccb6decba023d881c52ea111275d69bb1cae5
Wes Winham <winhamwr@gmail.com>
2009-06-30 12:06:03 -0400

    Added python packaging

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from distutils.core import setup
+ 
+def find_packages(root):
+    # so we don't depend on setuptools; from the Storm ORM setup.py
+    packages = []
+    for directory, subdirectories, files in os.walk(root):
+        if '__init__.py' in files:
+            packages.append(directory.replace(os.sep, '.'))
+    return packages
+ 
+setup(
+    name = 'django-ec2tools',
+    version = '0.1dev',
+    description = 'Maintenance helpers for your ec2-hosted django installation',
+    author = 'Wes Winham',
+    author_email = 'winhamwr@gmail.com',
+    license = 'BSD',
+    url = 'http://github.com/winhamwr/django-ec2tools',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+        ],
+    packages = find_packages('django_ec2tools'),
+    install_requires=['boto'],
+)
